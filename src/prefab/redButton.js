09e1cf9a0a2f5792d176bf6250d2d67af5d93648
Mansi S <mansisaini26@gmail.com>
2023-05-03 16:36:33 -0700

    fern is still working

--- src/prefab/redButton.js
+++ src/prefab/redButton.js
@@ -5,15 +5,10 @@ class Button extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
 
-        this.easy = difficulty
+        this.easy = difficulty;
 
-    }
+        this.effect = effect; 
 
-    preload() {
-
-        this.load.spritesheet('explosion', './assets/Rocket Patrol/explosion.png', {frameWidth: 64, frameHeight: 32, 
-          startFrame: 0, endFrame: 9});
-    
     }
 
     update() {
@@ -28,72 +23,46 @@ class Button extends Phaser.GameObjects.Sprite {
           this.alpha = 0;
     
           // create explosion sprite at ship's position
-          let boom = this.add.sprite(this.x, this.y, 'explosion').setOrigin(0, 0);
-          boom.anims.play('explode');     //that one anims you created earlier - play it
-          boom.on('animationcomplete', () => {
+          this.alpha = 1;
     
-              //ship.reset();       // reset ship to right of screen
-              boom.destroy();     // destroy explosion object
-              this.alpha = 1;
-    
-              if (this.easy) {
+            if (this.easy) {
     
                 game.settings = {
                   spaceshipSpeed: 3,
                   gameTimer: 60000    
                 }
     
-                this.sound.play('sfx_select');
-                this.scene.start('playScene'); 
-    
-              } else {
+            } else {
     
                 game.settings = {
                   spaceshipSpeed: 4,
                   gameTimer: 45000    
                 }
     
-                this.sound.play('sfx_select');
-                this.scene.start('playScene');  
+            }
     
-              }
-    
-          });
-    
-          this.sound.play('sfx_explosion');
+          };
     
         } else if (!rocket.bonked) {
     
           this.y += 5;
     
-          // create explosion sprite at ship's position
-          let boom = this.add.sprite(this.x, this.y, 'explosion').setOrigin(0, 0);
-          boom.alpha = 0;
-          boom.anims.play('explode');     //that one anims you created earlier - play it
-          boom.on('animationcomplete', () => {
+            // figure out the delay + animation
+
+          ship.y -= 5;
     
-              //ship.reset();       // reset ship to right of screen
-              boom.destroy();     // destroy explosion object
-              ship.y -= 5;
-    
-          });
+
         } else {
     
           this.y -= 5;
     
-          // create explosion sprite at ship's position
-          let boom = this.add.sprite(this.x, this.y, 'explosion').setOrigin(0, 0);
-          boom.alpha = 0;
-          boom.anims.play('explode');     //that one anims you created earlier - play it
-          boom.on('animationcomplete', () => {
+          // figure out the delay
+          
+          this.y += 5;
     
-              //ship.reset();       // reset ship to right of screen
-              boom.destroy();     // destroy explosion object
-              this.y += 5;
+        };
+        
     
-          });
-        }
-    
-      }
+    }
 
 }
