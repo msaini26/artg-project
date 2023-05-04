2afbf1b8094524c911fd88263bd56228e6da8d9a
Mansi S <mansisaini26@gmail.com>
2023-05-03 18:38:44 -0700

    ground moves

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -30,19 +30,20 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', './assets/characters/duck.png'); // import duck image
         this.load.image('spaceship', './assets/characters/spaceship.png'); // import spaceship
         this.load.image('swordfish', './assets/characters/swordfish.png'); // import speed rocket (swordfish)
+        this.load.image('redButton', './assets/buttons/button-2.png.png');
+        this.load.image('rocket', './assets/characters/duck.png');
         this.load.spritesheet('explosion', './assets/spritesheets/explosion.png', {frameWidth: 64, frameHeight: 32, 
             startFrame: 0, endFrame: 9});
     }
 
     create() {      // happens exactly once at beginning
 
-        // console.log("no bug here");
-
     // the set-up
-
         // import background
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'sky').setOrigin(0, 0); // sky background as the 'starfield'
-        this.ground = this.add.tileSprite(0, 0, 640, 680, 'ground').setOrigin(0, 0); // game ground asset
+        this.ground = this.add.tileSprite(0, 90, 640, 480, 'ground').setOrigin(0, 0); // game ground asset
+
+        this.plat1 = new Button(this, game.config.width/4, borderUISize*6 + borderPadding*4, 'redButton', 0, true).setOrigin(0, 0);
 
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
@@ -78,51 +79,45 @@ class Play extends Phaser.Scene {
         this.fireUI.alpha = 0;
 
         // 60-second play clock
-        this.gameOver = false;      // flag for game over state
+        // this.gameOver = false;      // flag for game over state
 
-        scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
+        // scoreConfig.fixedWidth = 0;
+        // this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
 
-            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER, LOSER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Try Again or <- for Menu!', scoreConfig).setOrigin(0.5);
-            this.gameOver = true;
+        //     this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER, LOSER', scoreConfig).setOrigin(0.5);
+        //     this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Try Again or <- for Menu!', scoreConfig).setOrigin(0.5);
+        //     this.gameOver = true;
 
-        }, null, this);
+        // }, null, this);
 
-        // UI - clock
-        this.remainingTime = game.settings.gameTimer/1000;
-        this.clockUI = this.add.text(borderUISize + borderUISize*15, borderUISize + borderPadding*2, this.remainingTime, scoreConfig);
+        // // UI - clock
+        // this.remainingTime = game.settings.gameTimer/1000;
+        // this.clockUI = this.add.text(borderUISize + borderUISize*15, borderUISize + borderPadding*2, this.remainingTime, scoreConfig);
     
     // the player
-
         // add rocket (p1)
-        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize*2 - borderPadding*2 - 110, 'rocket').setOrigin(0.5, 0);
 
         // define keys for controls
 
-        keyF = 
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-        keyR = 
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-        keyLEFT = 
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = 
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        keyDOWN = 
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        keyF=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyDOWN=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 
         // this.mouseActive = false;
 
     // the enemy
 
         // add spaceships (x3) + my small boy
-        this.swordfish = new smallBoy(this, game.config.width, borderUISize*6 + borderPadding*3, 'swordfish', 0, 60).setOrigin(0, 0);
+        // this.swordfish = new smallBoy(this, game.config.width, borderUISize*6 + borderPadding*3, 'swordfish', 0, 60).setOrigin(0, 0);
 
-        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        // this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         
-        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0, 0);
+        // this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0, 0);
         
-        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0);
+        // this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0);
 
         // animation config for explosion
         this.anims.create({
@@ -155,141 +150,141 @@ class Play extends Phaser.Scene {
 
     update() {      // does once every frame
 
-    // time
-        this.remainingTime = Math.floor(this.clock.getRemainingSeconds());
-        this.clockUI.text = this.remainingTime;
+    // // time
+    //     this.remainingTime = Math.floor(this.clock.getRemainingSeconds());
+    //     this.clockUI.text = this.remainingTime;
 
-    // restart check
-        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
-            this.scene.restart();
-        }
+    // // restart check
+    //     if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+    //         this.scene.restart();
+    //     }
 
-        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-            this.scene.start("menuScene");
-        }
+    //     if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+    //         this.scene.start("menuScene");
+    //     }
     
     // just the environment
-        this.starfield.tilePositionX -= 4;
+        this.ground.tilePositionX -= 0.5; // right - moving ground    
 
-    // sprite movements
-        if (!this.gameOver) {       // if game's still goin
+    // // sprite movements
+    //     if (!this.gameOver) {       // if game's still goin
 
-            if (!this.p1Rocket.grounded) {
-                this.fireUI.alpha = 1;
-            } else {
-                this.fireUI.alpha = 0;
-            }
+    //         if (!this.p1Rocket.grounded) {
+    //             this.fireUI.alpha = 1;
+    //         } else {
+    //             this.fireUI.alpha = 0;
+    //         }
 
-            this.p1Rocket.update();
+    //         this.p1Rocket.update();
 
-            this.ship01.update();
-            this.ship02.update();
-            this.ship03.update();
+    //         this.ship01.update();
+    //         this.ship02.update();
+    //         this.ship03.update();
 
-            this.swordfish.update();
+    //         this.swordfish.update();
         
-        }
+    //    }
 
     // check collisions
         
-        this.collisionWrapper(this.p1Rocket, this.ship03);
+        // this.collisionWrapper(this.p1Rocket, this.ship03);
         
-        this.collisionWrapper(this.p1Rocket, this.ship02);
+        // this.collisionWrapper(this.p1Rocket, this.ship02);
 
-        this.collisionWrapper(this.p1Rocket, this.ship01);
+        // this.collisionWrapper(this.p1Rocket, this.ship01);
 
-        this.collisionWrapper(this.p1Rocket, this.swordfish);
+        // this.collisionWrapper(this.p1Rocket, this.swordfish);
 
     }
 
-    checkCollision(rocket, ship) {
+    // checkCollision(rocket, ship) {
 
-        // simple AABB checking
-        if (rocket.x < ship.x + ship.width &&       // check if rocket origin is to left of ship's RIGHT bound
-            rocket.x + rocket.width > ship.x &&     // check if ship origin is to left of ROCKET'S RIGHT bound
-            rocket.y < ship.y + ship.height &&      // check if rocket origin is above ship's LOWER bound
-            rocket.height + rocket.y > ship. y) {   // check if ship origin is above ROCKET'S LOWER bound
+    //     // simple AABB checking
+    //     if (rocket.x < ship.x + ship.width &&       // check if rocket origin is to left of ship's RIGHT bound
+    //         rocket.x + rocket.width > ship.x &&     // check if ship origin is to left of ROCKET'S RIGHT bound
+    //         rocket.y < ship.y + ship.height &&      // check if rocket origin is above ship's LOWER bound
+    //         rocket.height + rocket.y > ship. y) {   // check if ship origin is above ROCKET'S LOWER bound
             
-            if (rocket.y > ship.y && !rocket.peaked){   // if hit ship's underside
-                rocket.bonked = true;
-            } else {
-                rocket.bonked = false;
-            }
+    //         if (rocket.y > ship.y && !rocket.peaked){   // if hit ship's underside
+    //             rocket.bonked = true;
+    //         } else {
+    //             rocket.bonked = false;
+    //         }
             
-            return true;
+    //         return true;
 
-          } else {
-            return false;
-          }
+    //       } else {
+    //         return false;
+    //       }
 
-    }
+    // }
 
-    addTime(miliseconds) {
+    // addTime(miliseconds) {
 
-        this.clock.delay += miliseconds;
+    //     this.clock.delay += miliseconds;
 
-    }
-    shipExplode(rocket, ship) {
+    // }
+    // shipExplode(rocket, ship) {
 
-        // increment timer
-        if (rocket.bouncing) {  // if combo started
+    //     // increment timer
+    //     if (rocket.bouncing) {  // if combo started
 
-            console.log("multiplier:", rocket.timeMultiplier)
-            this.addTime(ship.miliseconds * rocket.timeMultiplier);
+    //         console.log("multiplier:", rocket.timeMultiplier)
+    //         this.addTime(ship.miliseconds * rocket.timeMultiplier);
 
-        }
-        // temporarily hide ship
-        ship.alpha = 0;
+    //     }
+    //     // temporarily hide ship
+    //     ship.alpha = 0;
 
-        // create explosion sprite at ship's position
-        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
-        boom.anims.play('explode');     //that one anims you created earlier - play it
-        ship.reset();       // reset ship to right of screen
-        ship.alpha = 1;     // reset ship visibility to full
+    //     // create explosion sprite at ship's position
+    //     let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+    //     boom.anims.play('explode');     //that one anims you created earlier - play it
+    //     ship.reset();       // reset ship to right of screen
+    //     ship.alpha = 1;     // reset ship visibility to full
             
-        boom.on('animationcomplete', () => {
+    //     boom.on('animationcomplete', () => {
 
-            boom.destroy();     // destroy explosion object
+    //         boom.destroy();     // destroy explosion object
 
-        });
+    //     });
 
-        // score incrementation
-        rocket.score += ship.points * rocket.pointMultiplier;
-        this.scoreLeft.text = rocket.score;
+    //     // score incrementation
+    //     rocket.score += ship.points * rocket.pointMultiplier;
+    //     this.scoreLeft.text = rocket.score;
 
-        this.sound.play('sfx_explosion');
+    //     this.sound.play('sfx_explosion');
 
-    }
+    // }
 
-    collisionWrapper(rocket, ship){
+    // collisionWrapper(rocket, ship){
 
-        if (this.checkCollision(rocket, ship)) {
+    //     if (this.checkCollision(rocket, ship)) {
             
-            if (!rocket.bonked && !rocket.dropping) {
-                rocket.bouncing = true;
-                rocket.smallJump();
-            } else if (rocket.bonked) {
-                rocket.bonk();
-            }
+    //         if (!rocket.bonked && !rocket.dropping) {
+    //             rocket.bouncing = true;
+    //             rocket.smallJump();
+    //         } else if (rocket.bonked) {
+    //             rocket.bonk();
+    //         }
 
-            this.shipExplode(rocket, ship);
+    //         this.shipExplode(rocket, ship);
 
-        }
+    //     }
 
-    }
+    // }
 
-    shipsSpeedUp(){
+    // shipsSpeedUp(){
 
-        console.log("speeding up!");
-        this.ship01.moveSpeed *= 1.5;
-        this.ship02.moveSpeed *= 1.5;
-        this.ship03.moveSpeed *= 1.5;
-        this.swordfish.moveSpeed *= 1.5;
+    //     console.log("speeding up!");
+    //     this.ship01.moveSpeed *= 1.5;
+    //     this.ship02.moveSpeed *= 1.5;
+    //     this.ship03.moveSpeed *= 1.5;
+    //     this.swordfish.moveSpeed *= 1.5;
 
-    }
+    // }
 
-    swordfishSpawn(){
-        this.swordfish.go = true;
-    }
+    // swordfishSpawn(){
+    //     this.swordfish.go = true;
+    // }
 
 }
